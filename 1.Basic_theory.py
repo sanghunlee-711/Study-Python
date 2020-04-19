853d0a3f1a50f87b58e0f4b81b40ed50ff17368e
sanghunlee-711 <57477415+sanghunlee-711@users.noreply.github.com>
2020-04-19 22:40:43 +0900

    1st lecture
    int and snake

---
+++ 1.Basic_theory.py
@@ -0,0 +1,19 @@
+"""
+a = "like this"
+b = 3
+c = False /* false is not text false = 0, true = 1*/
+
+on python true -> True
+false -> False
+
+"""
+
+""" 여러줄 주석 """
+# 한줄주석 
+
+a_string = "Like this"
+a_number = 3
+a_float = 3.12 # float is number 3 != 3.12
+a_boolean = False 
+a_none = None """ not false but not exit, there is no boolean in none it's like NULL"""
+print(type(a_number))
