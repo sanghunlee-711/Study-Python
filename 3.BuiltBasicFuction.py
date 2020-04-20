a7641dc9e8754e0af3cbdc3678a98498b5d43e56
sanghunlee-711 <57477415+sanghunlee-711@users.noreply.github.com>
2020-04-20 20:38:01 +0900

    Create 3.BuiltBasicFuction.py
    Using Built in fuction

---
+++ 3.BuiltBasicFuction.py
@@ -0,0 +1,28 @@
+age = "18"
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))
+
+"""age = "18"
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))"""
+
+age = "18"
+print(age)
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))
+
+
+"""
+age = "18"
+print(age)
+print(type(age))
+n_age = int(age)
+print(n_age)
+print(type(n_age))"""
+
