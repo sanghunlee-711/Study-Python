9c8845c7a969108a2a1a1e939a6dc64b689877b4
sanghunlee-711 <57477415+sanghunlee-711@users.noreply.github.com>
2020-04-20 20:28:30 +0900

    Create 2.tuple.py
    string,number,boolean,None,list,tuple,dictionary

---
+++ 2.tuple.py
@@ -0,0 +1,28 @@
+"""days = ("Mon", "Tue","Wed","Thur","Fri") # []->() make it tuple
+print(type(days))"""
+
+# (tuple), [List], {Dictionary}
+
+nico = {
+"name": "Nico",
+"age": 29,
+"korea": True,
+"fav_food": ["Kimchi", "Sashimi"]
+}
+
+print(nico)
+nico["handsome"] = True
+print(nico)
+
+"""# (tuple), [List], {Dictionary}
+
+nico = {
+"name": "Nico",
+"age": 29,
+"korea": True,
+"fav_food": ["Kimchi", "Sashimi"]
+}
+
+print(nico)
+nico["handsome"] = True
+print(nico) """
