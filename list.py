48e3621c73f3c771ceb2ce1dd661c0530dabd154
sanghunlee-711 <57477415+sanghunlee-711@users.noreply.github.com>
2020-04-20 20:07:59 +0900

    Create list.py
    https://docs.python.org
    [string to list,
    mutalbe,Immutable,]

---
+++ list.py
@@ -0,0 +1,23 @@
+days = "Mon,Tue,Wed,Thur,Fri,Sat" #string
+print(days)
+"""try to find third day(Wed)"""
+days = ["Mon","Tue","Wed","Thur","Fri","Sat"] #list 
+"""string -> list"""
+
+"""
+days = ["Mon", "Tue","Wed","Thur","Fri"]
+
+print("Mon" in days)/// use Python dictionart in Google https://docs.python.org/3/""
+
+days = ["Mon", "Tue","Wed","Thur","Fri"]
+
+print(days[3])
+""" Thur gonnabe printed out because it start wiht 0 ~"""
+days = ["Mon", "Tue","Wed","Thur","Fri"]
+
+print(days)
+days.append("Sat")
+days.reverse()
+print(days)
+
+"""mutuable mean can change """
