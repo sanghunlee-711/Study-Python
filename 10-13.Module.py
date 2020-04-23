42881c2b7ed09a4e803612ea5134ba6ede3445db
sanghunlee-711 <57477415+sanghunlee-711@users.noreply.github.com>
2020-04-23 20:44:20 +0900

    Update 10-3.Module.py
    Theory done.

--- 10-13.Module.py
+++ 10-13.Module.py
@@ -11,3 +11,30 @@ from math import ceil, fsum
 """이렇게 뽑아오면 됨 ㅋ"""
 print(ceil(1.2))
 print(fsum([1, 2, 3, 4, 5, 6, 7]))
+
+
+from math import ceil, fsum as sexy_sum
+
+
+print(sexy_sum([1, 2, 3, 4, 5, 6, 7]))
+
+"""
+이런식으로 import하는 함수 이름을 바꿀 수 도 있음.
+"""
+"""
+ex)
+1. main.py
+from calculator import plus, minus
+
+print(plus(1,2), minus(1,2))
+
+2.calculator.py
+
+def plus(a, b):
+  return a + b
+
+
+def minus(a,b):
+  return a - b
+"""
+# 위와 같이 다른 py에서 정의한 함수를 끌어올 수 있음!.
